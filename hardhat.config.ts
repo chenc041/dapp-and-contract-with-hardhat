2b5393553539f0024af4ccbf697ed6835c950e74
chenc <double_cl@163.com>
2022-10-27 21:49:01 +0800

    chore: add template code

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,24 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotEnv from 'dotenv'
+
+dotEnv.config();
+const { API_URL, PRIVATE_KEY, TEST_API_URL, TEST_PRIVATE_KEY  } = process.env;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  defaultNetwork: "goerli",
+  networks: {
+    hardhat: {},
+    goerli: {
+      url: TEST_API_URL,
+      accounts: [`0x${TEST_PRIVATE_KEY}`]
+    },
+    mainnet: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  },
 };
 
 export default config;
